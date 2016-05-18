380eed2f7423d211285aed3a0162e5c4e164ad78
tarena <tarena@zhaoshijie.local>
2016-05-18 15:00:20 +0800

    Initial Commit

---
+++ SIXPoetryFate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SIXPoetryFate
+//
+//  Created by tarena on 16/5/18.
+//  Copyright © 2016年 six. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SIXPoetryFate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SIXPoetryFate
+//
+//  Created by tarena on 16/5/18.
+//  Copyright © 2016年 six. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
